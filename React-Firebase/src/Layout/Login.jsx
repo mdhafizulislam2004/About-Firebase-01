c2bd096a13c15f0f68aa0c4d9547990ea46f189f
Hafizul Islam <hafizul2004official@gmail.com>
2026-07-01 22:06:22 +0600

    signin Btn & Sign Out Btn Add & Login Section Update

--- React-Firebase/src/Layout/Login.jsx
+++ React-Firebase/src/Layout/Login.jsx
@@ -1,27 +1,50 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { FaGoogle } from "react-icons/fa";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../Firebase/Firebase.config";
-
+import { useState } from "react";
 
 const provider=new GoogleAuthProvider()
 
 const Login = () => {
 
-    const handleGoogleSignin=()=>{
-        
+    const [user,setUser]=useState(null)
+
+    const googleHendaler=()=>{
         signInWithPopup(auth,provider)
         .then(result=>{
-            console.log(result);
+            console.log(result.user);
+            setUser(result.user)
         })
         .catch(error=>{
             console.log(error);
         })
     }
-    
+
+    const signout=()=>{
+        signOut(auth)
+        .then(()=>{
+            console.log("Sign Out Done");
+            setUser(null)
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
     return (
         <div>
-            <h1>Login Page</h1>
-            <button onClick={handleGoogleSignin}><FaGoogle />Sign In With Google</button>
+            <h1>Login Section</h1>
+            {/* <button onClick={googleHendaler}>Login With Google</button>
+            <button onClick={signout}>Sign Out</button> */}
+
+            {
+                user?<button onClick={signout}>Sign Out</button>:<button onClick={googleHendaler}>Login With Google</button>
+            }
+            
+        {user && <div>
+            <h3>Name: {user.displayName}</h3>
+            <h4>Email: {user.email}</h4>
+            <img src={user.photoURL} alt="" />
+        </div>}
         </div>
     );
 };
